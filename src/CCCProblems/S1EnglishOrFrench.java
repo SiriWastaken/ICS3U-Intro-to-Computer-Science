fe3ae116abaf0354ff71b6c49e00286775030514
Sri <sriganty@icloud.com>
2026-07-02 14:03:56 -0400

    solved S1

---
+++ src/CCCProblems/S1EnglishOrFrench.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class S1EnglishOrFrench {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int textNumLines = input.nextInt();
+        int countT = 0;
+        int countS = 0;
+
+
+        for(int i = 0; i <= textNumLines; i++){
+            String textLine = input.nextLine();
+            String[] characterArray = textLine.split("");
+            for(int k = 0; k < characterArray.length; k++){
+                if(characterArray[k].equals("T") || characterArray[k].equals("t")){
+                    ++countT;
+                }
+                else if(characterArray[k].equals("S") || characterArray[k].equals("s")){
+                    ++countS;
+                }
+            }
+        }
+        if(countT > countS){
+            System.out.println("English");
+        }
+        else if(countS > countT || countS == countT){
+            System.out.println("French");
+        }
+    }
+}
