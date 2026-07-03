97fee6375cc75430222b49995913a2155420b0fc
Sri <sriganty@icloud.com>
2026-07-03 11:44:23 -0400

    FINALLY SOLVED PARTY INVITE

---
+++ src/cccPractice/CCCS1PartyInvite2014.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class CCCS1PartyInvite2014 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int numOfFriends = Integer.parseInt(input.readLine());
+        int numOfEliminations = Integer.parseInt(input.readLine());
+
+        ArrayList<Integer> friends = new ArrayList<>();
+        for (int i = 1; i <= numOfFriends; i++) {
+            friends.add(i);
+        }
+
+        for (int j = 0; j < numOfEliminations; j++) {
+            int eliminationMultiple = Integer.parseInt(input.readLine());
+
+            for (int i = friends.size() - 1; i >= 0; i--) {
+                if ((i + 1) % eliminationMultiple == 0) {
+                    friends.remove(i);
+                }
+            }
+        }
+        for (int remainingFriend : friends) {
+            System.out.println(remainingFriend);
+        }
+    }
+}
---
+++ src/cccPractice/CCCS1SprinterSpeed2020.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class CCCS1SprinterSpeed2020 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+
+        int numOfInputs = Integer.parseInt(input.readLine());
+        
+        int[][] data = new int[numOfInputs][2];
+
+        for (int i = 0; i < numOfInputs; i++) {
+            String[] tokens = input.readLine().split(" ");
+            data[i][0] = Integer.parseInt(tokens[0]); 
+            data[i][1] = Integer.parseInt(tokens[1]); 
+        }
+
+        Arrays.sort(data, (a, b) -> Integer.compare(a[0], b[0]));
+
+        double maxSpeed = 0;
+        for (int i = 0; i < numOfInputs - 1; i++) {
+            double distance = Math.abs(data[i + 1][1] - data[i][1]);
+            double timeDiff = data[i + 1][0] - data[i][0];
+            
+            double speed = distance / timeDiff;
+            if (speed > maxSpeed) {
+                maxSpeed = speed;
+            }
+        }
+
+        System.out.println(maxSpeed);
+    }
+}
